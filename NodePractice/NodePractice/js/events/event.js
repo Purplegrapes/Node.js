e77983a4a5bf39a7a6c68ccf5cef985d5ac07608
琼 张 <zhangqiong1995@outlook.com>
2016-09-21 16:47:38 +0800

    node1

---
+++ NodePractice/NodePractice/js/events/event.js
@@ -0,0 +1,6 @@
+﻿var EventEmitter = require('events').EventEmitter;
+var life = new EventEmitter();
+life.on('d', function (names) {
+    console.log('a'+names+'c')
+})
+life.emit('d','june')
---
+++ NodePractice/NodePractice/js/http/Scope.js
@@ -0,0 +1,8 @@
+﻿var globalVariable = 'This is global';
+function globalFunction() {
+    var LocalVariable = 'This is local';
+    console.log("ddd");
+    console.log(globalVariable);
+    console.log(LocalVariable);
+}
+globalFunction();
